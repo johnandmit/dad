63a0870711801324289c597c224d83ddc4ed5ecf
johnandmit <95743485+johnandmit@users.noreply.github.com>
2023-01-11 11:44:58 +0700

    Create treeThingy.c

---
+++ treeThingy.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Tnode
+{
+    int id;
+    struct Tnode *LeftMostChild;
+    struct Tnode *rightSibling;
+};
+typedef struct Tnode treeNode;
+
+treeNode* parent(treeNode *T, treeNode *n, treeNode *parent)
+{
+    parent(T->LeftMostChild,n,T);
+    parent(T->rightSibling,n,parent);
+}
+
+int main()
+{
+    treeNode *root =  (treeNode*) malloc(sizeof(treeNode));
+    treeNode *node1 = (treeNode*) malloc(sizeof(treeNode));
+    treeNode *node2 = (treeNode*) malloc(sizeof(treeNode));
+    treeNode *node3 = (treeNode*) malloc(sizeof(treeNode));
+    treeNode *node4 = (treeNode*) malloc(sizeof(treeNode));
+    treeNode *node5 = (treeNode*) malloc(sizeof(treeNode));
+    treeNode *node6 = (treeNode*) malloc(sizeof(treeNode));
+    treeNode *node7 = (treeNode*) malloc(sizeof(treeNode));
+    treeNode *node8 = (treeNode*) malloc(sizeof(treeNode));
+    treeNode *node9 = (treeNode*) malloc(sizeof(treeNode));
+
+    root->id = 0;//a
+    node1->id = 1;//b
+    node2->id = 2;//c
+    node3->id = 3;//d
+    node4->id = 4;//e
+    node5->id = 5;//f
+    node6->id = 6;//g
+    node7->id = 7;//h
+    node8->id = 8;//i
+    node9->id = 9;//j
+
+    root->rightSibling = NULL; root->LeftMostChild = node1;
+
+    node1->rightSibling = node2; node1->LeftMostChild = node3;
+
+    node2->LeftMostChild = node5; node2->rightSibling = NULL;
+
+    node3->rightSibling = node4; node3->LeftMostChild = NULL;
+
+    node5->LeftMostChild = node5; node5->rightSibling = node9;
+
+    node6->rightSibling = node7; node6->LeftMostChild = NULL;
+
+    node7->rightSibling = node8; node7->LeftMostChild = NULL;
+
+    printf("%i",parent(root,node2,root)->id);
+}
