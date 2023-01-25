22f87788b2fee4d1a67ed6231dc288cf1028eb39
johnandmit <95743485+johnandmit@users.noreply.github.com>
2023-01-25 19:27:22 +0700

    Update bai 24 2.c

--- bai 24 2.c
+++ bai 24 2.c
@@ -1,52 +1,66 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct Tnode {
-  int id;
-  struct Tnode *left;
-  struct Tnode *right;
+struct Tnode
+{
+    int id;
+    struct Tnode *left;
+    struct Tnode *right;
 };
 typedef struct Tnode treeNode;
 
-treeNode *makeTreeNode(int x, treeNode *parent) {
-  treeNode *temp = (treeNode *)malloc(sizeof(treeNode));
-  temp->id = x;
-  temp->left = NULL;
-  temp->right = NULL;
-  if (parent != NULL) {
-    if (parent->left == NULL) {
-      parent->left = temp;
-    } else if (parent->right == NULL) {
-      parent->right = temp;
-    } else {
-      printf("node with label %i can't have any more child", parent->id);
+treeNode *makeTreeNode(int x, treeNode *parent)
+{
+    treeNode *temp = (treeNode *)malloc(sizeof(treeNode));
+    temp->id = x;
+    temp->left = NULL;
+    temp->right = NULL;
+    if (parent != NULL)
+    {
+        if (parent->left == NULL)
+        {
+            parent->left = temp;
+        }
+        else if (parent->right == NULL)
+        {
+            parent->right = temp;
+        }
+        else
+        {
+            printf("node with label %i can't have any more child", parent->id);
+        }
     }
-  }
-  return temp;
+    return temp;
 }
 
-int treePath(int x, treeNode *leaf, treeNode *root, int total) {
-  if (root == NULL)
-    return 0;
-  if (root == leaf) 
-	{
-    if (total == x) {
-      return 1;
+int treePath(int x, treeNode *leaf, treeNode *root, int total)
+{
+    if (root == NULL)
+    {
+        return 0;
     }
-    return 0;
-  }
-  if (treePath(x, leaf, root->left, root->id + total) || treePath(x, leaf, root->right, root->id + total)) {
-    printf("e");
-    return 1;
-  }
+    total+=root->id;
+    if (root == leaf)
+    {
+        if (total == x)
+        {
+            return 1;
+        }
+        return 0;
+    } 
+    if (treePath(x, leaf, root->left, total) || treePath(x, leaf, root->right,total))
+    {
+        return 1;
+    }
 }
 
-int main() {
-  treeNode *root = makeTreeNode(1, NULL);
-  treeNode *node1 = makeTreeNode(2, root);
-  treeNode *node2 = makeTreeNode(3, root);
-  treeNode *node3 = makeTreeNode(4, node2);
-  treeNode *node4 = makeTreeNode(5, node1);
-  treeNode *node5 = makeTreeNode(6, node1);
-  printf("%i", treePath(8, node4, root, 0));
+int main()
+{
+    treeNode *root = makeTreeNode(1, NULL);
+    treeNode *node1 = makeTreeNode(2, root);
+    treeNode *node2 = makeTreeNode(3, root);
+    treeNode *node3 = makeTreeNode(4, node1);
+    treeNode *node4 = makeTreeNode(5, node1);
+    treeNode *node5 = makeTreeNode(6, node2);
+    printf("%i", treePath(4, node2, root, 0));
 }
