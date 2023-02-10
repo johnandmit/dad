d80b3820519ab4860ada8817375ade72b1b23566
johnandmit <95743485+johnandmit@users.noreply.github.com>
2023-02-10 21:19:11 +1300

    Create bai 26 3.c

---
+++ bai 26 3.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Tnode
+{
+    int id;
+    struct Tnode *left;
+    struct Tnode *right;
+};
+typedef struct Tnode treeNode;
+
+int total = 0;
+
+treeNode *makeTreeNode(int x, treeNode *parent)
+{
+    treeNode *temp = (treeNode *)malloc(sizeof(treeNode));
+    temp->id = x;
+    temp->left = NULL;
+    temp->right = NULL;
+    if (parent != NULL)
+    {
+        if (parent->left == NULL)
+        {
+            parent->left = temp;
+        }
+        else if (parent->right == NULL)
+        {
+            parent->right = temp;
+        }
+        else
+        {
+            printf("node with label %i can't have any more child", parent->id);
+        }
+    }
+    return temp;
+}
+
+void treePath(int k, treeNode *root)
+{
+    if (root != NULL)
+    {
+        if (root->id > k)
+        {
+            total++;
+        }
+        treePath(k, root->left);
+        treePath(k, root->right);
+    }
+}
+
+int main()
+{
+    treeNode *root = makeTreeNode(1, NULL);
+    treeNode *node1 = makeTreeNode(2, root);
+    treeNode *node2 = makeTreeNode(3, root);
+    treeNode *node3 = makeTreeNode(4, node1);
+    treeNode *node4 = makeTreeNode(5, node1);
+    treeNode *node5 = makeTreeNode(6, node2);
+    treePath(4, root);
+    printf("%i",total);
+}
